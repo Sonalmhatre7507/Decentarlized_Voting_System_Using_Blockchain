293490381f07b048b0b2d090b78ba7c095bcc465
Sonalmhatre7507 <91831838+Sonalmhatre7507@users.noreply.github.com>
2023-10-31 15:21:42 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,22 @@
-"""Election URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', views.home, name='home'),
+    path('authentication/', views.authentication, name='authentication'),
+    path('get-parties/', views.get_parties, name='get-parties'),
+    path('create-vote/', views.create_vote, name='create-vote'),
+    path('send-otp/', views.send_otp, name='send-otp'),
+    path('verify-otp/', views.verify_otp, name='verify-otp'),
+    path('create-dummy-data/', views.create_dummy_data, name='create-dummy-data'),
+    path('show-result/', views.show_result, name='show-result'),
+    path('mine-block/', views.mine_block, name='mine-block'),
+    path('start-mining/', views.start_mining, name='start-mining'),
+    path('blockchain/', views.blockchain, name='blockchain'),
+    path('block-info/', views.block_info, name='block-info'),
+    path('sync-block/', views.sync_block, name='sync-block'),
+    path('verify-block/', views.verify_block, name='verify-block'),
+    path('track-server/', views.track_server, name='track-server'),
 ]
